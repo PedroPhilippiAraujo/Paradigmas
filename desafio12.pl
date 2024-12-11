1a8a353269b03cc4405cba1ea27a0d2f9fe1acbd
Pedro Araujo <pedro@syncnow.net>
2024-12-11 15:52:42 -0300

    Add files via upload

--- desafio12.pl
+++ desafio12.pl
@@ -65,67 +65,79 @@ solucao(ListaSolucao) :-
     ],
 
     %A mulher que esqueceu o Amaciante está exatamente à esquerda da que foi dirigindo um Sedan.
-    %??? aolado+aesquerda
-    
-    %Juliana foi ao supermercado com a Mãe.
-    member(caixa(_,juliana,_,_,mae,_), ListaSolucao),
+    aEsquerda(caixa(_,_,amaciante,_,_,_), caixa(_,_,_,_,_,sedan), ListaSolucao),
+    aoLado(caixa(_,_,amaciante,_,_,_), caixa(_,_,_,_,_,sedan), ListaSolucao),
     
     %Quem foi num Crossover está exatamente à direita de quem vai pagar no cartão de Débito.
-    %??? aolado+adireita
-    
-    %Quem esqueceu o Presunto vai pagar com o cartão de Débito.
-    member(caixa(_,_,presunto,debito,_,_), ListaSolucao),
+    aDireita(caixa(_,_,_,_,_,crossover), caixa(_,_,_,debito,_,_), ListaSolucao),
+    aoLado(caixa(_,_,_,_,_,crossover), caixa(_,_,_,debito,_,_), ListaSolucao),
     
     %A mulher que foi com o Namorado foi fazer as compras dirigindo uma Pickup.
     member(caixa(_,_,_,_,namorado,pickup), ListaSolucao),
     
-    %A mulher da blusa Amarela está ao lado da que esqueceu as Frutas.
-    aoLado(caixa(amarela,_,_,_,_,_), caixa(_,_,frutas,_,_,_), ListaSolucao),
-    
     %A dona do Sedan está exatamente à esquerda da dona do SUV.
-    %???
-    
-    %Quem esqueceu o Pão foi ao supermercado dirigindo um SUV.
-    member(caixa(_,_,pao,_,_,suv), ListaSolucao),
+    %aesqueda+aolado
+    aEsquerda(caixa(_,_,_,_,_,sedan), caixa(_,_,_,_,_,suv), ListaSolucao),
+    aoLado(caixa(_,_,_,_,_,sedan), caixa(_,_,_,_,_,suv), ListaSolucao),
     
     %Quem esqueceu o Pão foi com a Mãe.
     member(caixa(_,_,pao,_,mae,_), ListaSolucao),
     
-    %Fernanda foi para o supermercado com o Filho.
-    member(caixa(_,fernanda,_,_,filho,_), ListaSolucao),
-    
     %Aline está ao lado da mulher que foi ao supermercado com o Filho.
     aoLado(caixa(_,aline,_,_,_,_), caixa(_,_,_,_,filho,_), ListaSolucao),
-    
-    %Carol está exatamente à direita da mulher que esqueceu o Amaciante.
-    %???
-    
-    %A mulher da blusa Azul está no quarto caixa.
-    Blusa4 = azul,
     
     %Em um dos caixas da pontas está a mulher que foi ao supermercado com o Marido.
     noCanto(caixa(_,_,_,_,marido,_), ListaSolucao),
     
-    %A mulher da blusa Verde está em algum lugar à esquerda da de blusa Vermelha.
-    aEsquerda(caixa(verde,_,_,_,_,_), caixa(vermelha,_,_,_,_,_), ListaSolucao),
-    
     %No caixa da quarta posição está a mulher que vai pagar com Cheque.
     Pagamento4 = cheque,
     
-    %Quem vai pagar com Dinheiro está em um dos caixas das pontas.
-    noCanto(caixa(_,_,_,dinheiro_,_), ListaSolucao),
+    %Quem vai pagar com Dinheiro está em um dos caixas das pontas. ERRO NESSA REGRA
+    noCanto(caixa(_,_,_,dinheiro,_,_), ListaSolucao),
+    
+    %A mulher que vai pagar com o cartão de Débito está exatamente à esquerda de quem vai pagar com Vale.
+    aEsquerda(caixa(_,_,_,debito,_,_), caixa(_,_,_,vale,_,_), ListaSolucao),
+    aoLado(caixa(_,_,_,debito,_,_), caixa(_,_,_,vale,_,_), ListaSolucao),
+    
+    
+    %------------------------------------------------------------
+    %Juliana foi ao supermercado com a Mãe.
+    member(caixa(_,juliana,_,_,mae,_), ListaSolucao),
+    
+    %Quem esqueceu o Presunto vai pagar com o cartão de Débito.
+    member(caixa(_,_,presunto,debito,_,_), ListaSolucao),
+    
+    %A mulher da blusa Amarela está ao lado da que esqueceu as Frutas.
+    aoLado(caixa(amarela,_,_,_,_,_), caixa(_,_,frutas,_,_,_), ListaSolucao),
+    
+    %Quem esqueceu o Pão foi ao supermercado dirigindo um SUV.
+    member(caixa(_,_,pao,_,_,suv), ListaSolucao),
+    
+    %Fernanda foi para o supermercado com o Filho.
+    member(caixa(_,fernanda,_,_,filho,_), ListaSolucao),
+    
+    %Carol está exatamente à direita da mulher que esqueceu o Amaciante.
+    aDireita(caixa(_,carol,_,_,_,_), caixa(_,_,amaciante,_,_,_), ListaSolucao),
+    aoLado(caixa(_,carol,_,_,_,_), caixa(_,_,amaciante,_,_,_), ListaSolucao),
+    
+    %A mulher da blusa Azul está no quarto caixa.
+    Blusa4 = azul,
+    
+    %A mulher da blusa Verde está em algum lugar à esquerda da de blusa Vermelha.
+    aEsquerda(caixa(verde,_,_,_,_,_), caixa(vermelha,_,_,_,_,_), ListaSolucao),
     
     %Quem esqueceu as Frutas está ao lado de quem esqueceu o Presunto.
     aoLado(caixa(_,_,frutas,_,_,_), caixa(_,_,presunto,_,_,_), ListaSolucao),
     
-    %A mulher que vai pagar com o cartão de Débito está exatamente à esquerda de quem vai pagar com Vale.
-    %aesquerda+aolado.
-    
     %A mulher da blusa Amarela está em algum lugar entre a que foi com o Marido e a de blusa Verde, nessa ordem.
-    %adireita+aesquerda
+    %adireita(marido)+aesquerda(verde)
+    aDireita(caixa(amarela,_,_,_,_,_), caixa(_,_,_,_,marido,_), ListaSolucao),
+    aEsquerda(caixa(amarela,_,_,_,_,_), caixa(verde,_,_,_,_,_), ListaSolucao),
     
     %Quem vai pagar com Dinheiro está ao lado de quem foi de Sedan ao supermercado.
     aoLado(caixa(_,_,_,dinheiro,_,_), caixa(_,_,_,_,_,sedan), ListaSolucao),
+    
+    %-----------------------------------------------------------
     
     %Testa todas as possibilidades...
     blusa(Blusa1), blusa(Blusa2), blusa(Blusa3), blusa(Blusa4), blusa(Blusa5),
@@ -145,4 +157,3 @@ solucao(ListaSolucao) :-
 
     carro(Carro1), carro(Carro2), carro(Carro3), carro(Carro4), carro(Carro5),
     todosDiferentes([Carro1, Carro2, Carro3, Carro4, Carro5]).
-
